134efdfadb86275472c3f4d940e925b3d622f989
BRSantos85 <157507622+BRSantos85@users.noreply.github.com>
2024-02-12 18:57:55 -0600

    Version Final del Juego secreto en Alura

---
+++ EJercico.js
@@ -0,0 +1,6 @@
+let ListaGenerica = [];
+let LenguajesDeProgramacion = ["Javascript","C","C++","Kotlin","Python"]
+
+LenguajesDeProgramacion.push("Java","Ruby","GoLang");
+
+console.log(LenguajesDeProgramacion);
